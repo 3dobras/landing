2a35610aeb2f5e2d1a02ed59b5138e27e83b3673
3dobras <139887159+3dobras@users.noreply.github.com>
2023-07-18 11:38:48 -0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64b69612477c3a0019b5437b",
+				"64b69612477c3a0019b5437e"
+			]
+		},
+		"64b69612477c3a0019b5437b": {
+			"id": "64b69612477c3a0019b5437b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"64b69612477c3a0019b5437e": {
+			"id": "64b69612477c3a0019b5437e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
